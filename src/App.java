8759b2a64bc8f54521f88964ea4d2b50603bea3f
Nestor Bardel <nestorbardel@gmail.com>
2025-09-20 09:43:34 -0300

    Add Animal class and enhance Persona class with getter and setter methods

--- src/App.java
+++ src/App.java
@@ -2,8 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Persona persona1 = new Persona("Pedro", "Pascal", 48);
+        persona1.setNombre("Ricardo");
+        persona1.setApellido("Darin");
 
-        System.out.println(persona1.darNombreCompleto()+" tiene " + persona1.edad +" años.");
+        System.out.println(persona1.darNombreCompleto()+" tiene " + persona1.getEdad() +" años.");
 
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -17,4 +17,28 @@ public class Persona {
         if(this.edad > 40) return "Buenos dias, querido "+saludado;
         return "Hola, como estas " + saludado + "?";
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
